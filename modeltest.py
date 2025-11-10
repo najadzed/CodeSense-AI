9b86a7c3c272e63f5186ba05a7e56c2de9ab49c6
Najad <najaddh@gmail.com>
2025-11-10 22:37:46 +0530

    Delete modeltest.py

--- modeltest.py
+++
@@ -1,4 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyCNv8tIxzylhGincFxKbxHexsAtDwdWUgc")
-for m in genai.list_models():
-    print(m.name, " | supports generate_content:", "generateContent" in m.supported_generation_methods)
